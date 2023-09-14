b96a523d62d39b1316139b220525798d8b8f506f
Bryan Lecza <brylex0324@gmail.com>
2023-09-14 19:15:14 -0400

    Initial commit

---
+++ Source/RacketLeague/RacketLeagueGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RacketLeagueGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RACKETLEAGUE_API ARacketLeagueGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
